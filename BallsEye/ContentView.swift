93de9e6480bb40deea1e99560f13cb223a0d39d5
Danila Gorelko <gorelko.danila@gmail.com>
2020-04-03 18:34:32 +0300

    Initial Commit

---
+++ BallsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BallsEye
+//
+//  Created by Danila Gorelko on 3.4.20.
+//  Copyright © 2020 Danila Gorelko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
